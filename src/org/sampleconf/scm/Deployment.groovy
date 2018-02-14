4076cdb405ee7f85dbd6805f13e5d63a8c98a4ca
Sanjana.T <sanjana.t@Sanjanas-MacBook-Pro.local>
2018-02-14 08:26:51 +0530

    added file

---
+++ src/org/sampleconf/scm/Deployment.groovy
@@ -0,0 +1,24 @@
+package org.k9.openshift
+
+
+	class Deployment implements Serializable {
+	  def config
+	  def script
+
+
+	  Deployment(script, config) {
+	    this.config = config
+	    this.script = script
+	  }
+
+
+void deployFromJson() {
+	    
+	    openshift.withCluster('https://192.168.99.102:8443','oIDbWZt0qPxaG2k9rkPjCQUIbt1t7tlhv1tSEMEoYyw') {
+	    echo "Using project: ${openshift.project()}"
+      	    def objs = openshift.create(config.templatePath)
+             objs.describe()
+	} 
+    } 
+}
+
